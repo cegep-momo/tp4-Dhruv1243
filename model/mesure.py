5e3e02480329f738a040a4a02fb7b93bde6857ad
Dhruv Patel <pateld2253@gmail.com>
2025-04-20 15:19:35 -0400

    CRÉATI0N de la class mésure

--- model/mesure.py
+++ model/mesure.py
@@ -1,4 +1,17 @@
-
+from datetime import datetime
 
 class Mesure:
-    pass
+    def __init__(self, dateHeureMesure: str, dataMesure: list):
+        self.dateHeureMesure = dateHeureMesure
+        self.dataMesure = dataMesure
+        
+    def __repr__(self):
+        return f"{self.dateHeureMesure} | {self.dataMesure}"
+    
+    def afficherMesure(self):
+        msg = f"Voici la date complète : {self.dateHeureMesure}\n"
+        msg += "Voici la ou les valeur(s) :\n"
+        for i, val in enumerate(self.dataMesure, start=1):
+            msg += f"  - Valeur {i} : {val}\n"
+        return msg
+        
