b82a75f09340439d770028d25e609457222ba757
Dhruv Patel <pateld2253@gmail.com>
2025-04-24 09:21:19 -0400

    DÉBOGAGE ajusté l'affichage du text dans l'écran lcd

--- controler/controler.py
+++ controler/controler.py
@@ -4,99 +4,84 @@ from datetime import datetime
 from view.platine import Platine
 from model.mesure import Mesure
 import RPi.GPIO as GPIO
-from LCD1602 import CharLCD1602
+import LCD1602  
 
 class Controleur:
     def __init__(self):
-        
         self.platine = Platine()
-        self.lcd = CharLCD1602()
-        self.en_cours = False  
+        self.en_cours = False
+        LCD1602.init(0x27, 1)  
 
     def sauvegarder_mesure(self, mesure: Mesure):
-        
         donnees = {
-        "dateHeure": mesure.dateHeureMesure,
-        "valeur": mesure.dataMesure
+            "dateHeure": mesure.dateHeureMesure,
+            "valeur": mesure.dataMesure
         }
 
         try:
-           
             with open("mesuresTemp.json", "r") as f:
                 liste_mesures = json.load(f)
         except (FileNotFoundError, json.JSONDecodeError):
-            
             liste_mesures = []
 
-        
         liste_mesures.append(donnees)
 
-        
         with open("mesuresTemp.json", "w") as f:
             json.dump(liste_mesures, f, indent=4)
 
-        print("Mésure sauvegardée")
+        print("Mesure sauvegardee")
 
     def debutter_programme(self):
         try:
             while True:
-                
-                self.lcd.clear()
-                self.lcd.write(0, 0, "Appuyez pour")
-                self.lcd.write(1, 1, "demarrer...")
-                
+                LCD1602.clear()
+                LCD1602.write(0, 0, "Appuyez bouton")
+                LCD1602.write(1, 1, "pour demarrer")
+
                 self.en_cours = self.platine.attendre_bouton_debut_fin()
 
                 if self.en_cours:
-                    self.lcd.clear()
-                    self.lcd.write(0, 0, "Systeme commence")
+                    LCD1602.clear()
+                    LCD1602.write(0, 0, "Systeme demarre")
                     print("Systeme demarre")
 
                     while self.en_cours:
-                        
                         valeur = self.platine.lire_capteur()
- 
-                        self.lcd.clear()
-                        self.lcd.write(0, 0, "Temperature (C):")
-                        self.lcd.write(1, 1, str(valeur[0]))
-                        
-                        
-                        for _ in range(10):  
-                            
+
+                        LCD1602.clear()
+                        LCD1602.write(0, 0, "Temp (C):")
+                        LCD1602.write(1, 1, str(valeur[0]))
+
+                        for _ in range(10):
+                            # Si on appuie de nouveau pour arreter
                             if GPIO.input(self.platine.btn_debut_fin) == GPIO.LOW:
                                 self.en_cours = self.platine.attendre_bouton_debut_fin()
                                 if not self.en_cours:
-                                    self.lcd.clear()
-                                    self.lcd.write(0, 0, "Systeme")
-                                    self.lcd.write(1, 1, "arrete")
-                                    print("Système arrêté")
+                                    LCD1602.clear()
+                                    LCD1602.write(0, 0, "Systeme")
+                                    LCD1602.write(1, 1, "arrete")
+                                    print("Systeme arrete")
                                     time.sleep(2)
-                                    self.lcd.clear()
-                                    self.en_cours = False
+                                    LCD1602.clear()
                                     break
-                            
+
                             if self.platine.bouton_mesurer_appuye():
                                 valeur_manuelle = self.platine.lire_capteur()
                                 maintenant = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
                                 mesure = Mesure(maintenant, valeur_manuelle)
 
-                                self.lcd.clear()
-                                self.lcd.write(0, 0, "Mesure")
-                                self.lcd.write(1, 1, "Enregistre")
-                                print(f"Mesure enregistrée : {valeur_manuelle[0]}°C")
+                                LCD1602.clear()
+                                LCD1602.write(0, 0, "Mesure")
+                                LCD1602.write(1, 1, "enregistree")
+                                print(f"Mesure sauvée : {valeur_manuelle[0]}°C")
 
                                 self.sauvegarder_mesure(mesure)
                                 time.sleep(2)
 
                             time.sleep(0.5)
 
-                        
-                        
-                        
-
         except KeyboardInterrupt:
-            
-            self.lcd.clear()
-            self.lcd.write(0, 0, "Programme")
-            self.lcd.write(1, 1, "termine")
+            LCD1602.clear()
+            LCD1602.write(0, 0, "Programme")
+            LCD1602.write(1, 1, "termine")
             self.platine.cleanup()
