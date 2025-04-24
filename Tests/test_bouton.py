ac01c72c78ba147615a15d22be134951bbab5763
Dhruv Patel <pateld2253@gmail.com>
2025-04-24 08:54:12 -0400

    TEST unitaire sur le bouton-poussoir

---
+++ Tests/test_bouton.py
@@ -0,0 +1,21 @@
+import unittest
+from gpiozero import Device
+from gpiozero.pins.mock import MockFactory
+from view.platine import Platine
+
+Device.pin_factory = MockFactory()
+
+class TestPlatine(unittest.TestCase):
+    def setUp(self):
+        self.platine = Platine(pin_demarrage=17, pin_mesure=27)
+
+    def test_bouton_mesure_appuye(self):
+        self.platine.pin_mesure_device.pin.drive_low()
+        self.assertTrue(self.platine.bouton_mesurer_appuye())
+
+    def test_bouton_mesure_pas_appuye(self):
+        self.platine.pin_mesure_device.pin.drive_high()
+        self.assertFalse(self.platine.bouton_mesurer_appuye())
+
+if __name__ == '__main__':
+    unittest.main()
