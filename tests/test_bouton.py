53216fd21ff9d91de158a59faac077ebcf707c98
Dhruv Patel <pateld2253@gmail.com>
2025-04-24 09:08:57 -0400

    TEST ajouté une fonction pour fermer les btn après chaque tests

--- tests/test_bouton.py
+++ tests/test_bouton.py
@@ -7,15 +7,19 @@ Device.pin_factory = MockFactory()
 
 class TestPlatine(unittest.TestCase):
     def setUp(self):
-        self.platine = Platine(pin_demarrage=16, pin_mesure=26, mode_test=True)
+        self.platine = Platine(btn_debut_fin=16, btn_mesurer=26, test=True)
+
+    def tearDown(self):
+        self.platine.pin_mesure_device.close()
+        self.platine.pin_demarrage_device.close()
 
     def test_bouton_mesure_appuye(self):
         self.platine.pin_mesure_device.pin.drive_low()
-        self.assertTrue(self.platine.bouton_mesurer_appuye())
+        self.assertTrue(self.platine.pin_mesure_device.is_active)  
 
     def test_bouton_mesure_pas_appuye(self):
         self.platine.pin_mesure_device.pin.drive_high()
-        self.assertFalse(self.platine.bouton_mesurer_appuye())
+        self.assertFalse(self.platine.pin_mesure_device.is_active)
 
 if __name__ == '__main__':
     unittest.main()
